9f8ef7eb0ab6856628bbf69530264844db768c30
Craig Griffiths <craigsaint@outlook.com>
2026-06-20 03:58:36 +0100

    access control

--- PropertySurveyService/Data/DbInitializer.cs
+++ PropertySurveyService/Data/DbInitializer.cs
@@ -13,7 +13,11 @@ namespace PropertySurveyService.Data
         SuperAdmin,
         Admin,
         Moderator,
-        Basic
+        Basic,
+        Contract,
+        Branch,
+        Vehicle,
+        Staff
     }
     public static class DbInitializer
     {
@@ -23,7 +27,11 @@ namespace PropertySurveyService.Data
             await roleManager.CreateAsync(new IdentityRole(Roles.SuperAdmin.ToString()));
             await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
             await roleManager.CreateAsync(new IdentityRole(Roles.Moderator.ToString()));
-            await roleManager.CreateAsync(new IdentityRole(Roles.Basic.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Basic.ToString())); 
+            await roleManager.CreateAsync(new IdentityRole(Roles.Contract.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Branch.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Vehicle.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Staff.ToString()));
         }
 
         public static async Task SeedSuperAdminAsync(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
