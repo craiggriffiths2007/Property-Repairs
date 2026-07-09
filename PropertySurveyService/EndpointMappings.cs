5ba8214cc3c009ef2194194ff35a499277e8de99
Craig Griffiths <craigsaint@outlook.com>
2026-07-09 05:57:52 +0100

    receiving jobs images problem

--- PropertySurveyService/EndpointMappings.cs
+++ PropertySurveyService/EndpointMappings.cs
@@ -564,6 +564,7 @@ namespace PropertySurveyService
                         results.Add(new JobHeaderDTO
                         {
                             Head = jobHeader,
+                            Images = images
                         });
                     }
                 }
